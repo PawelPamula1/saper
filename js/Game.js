c9a8adb54cd2efaaeee337b18b9d9e252093ee1c
PawelPamula1 <pawelpamula003@gmail.com>
2021-10-05 19:06:06 +0200

    adding bombs, timer and counter functionality

--- js/Game.js
+++ js/Game.js
@@ -1,5 +1,7 @@
 import { Cell } from "./Cell.js";
 import { UI } from "./UI.js";
+import { Counter } from "./Counter.js";
+import { Timer } from "./Timer.js";
 
 class Game extends UI {
   #config = {
@@ -20,6 +22,9 @@ class Game extends UI {
     },
   };
 
+  #counter = new Counter();
+  #timer = new Timer();
+
   #numberOfRows = null;
   #numberOfCols = null;
   #numberOfMines = null;
@@ -31,6 +36,8 @@ class Game extends UI {
 
   initializeGame() {
     this.#handleElements();
+    this.#counter.init();
+    this.#timer.init();
     this.#newGame();
   }
 
@@ -42,6 +49,9 @@ class Game extends UI {
     this.#numberOfRows = rows;
     this.#numberOfCols = cols;
     this.#numberOfMines = mines;
+
+    this.#counter.setValue(this.#numberOfMines);
+    this.#timer.startTimer();
 
     this.#setStyles();
 
@@ -92,7 +102,17 @@ class Game extends UI {
     const colIndex = parseInt(target.getAttribute("data-x"), 10);
     const cell = this.#cells[rowIndex][colIndex];
     if (cell.isRevealed) return;
-    cell.toggleFlag();
+
+    if (cell.isFlagged) {
+      this.#counter.increment();
+      cell.toggleFlag();
+      return;
+    }
+
+    if (this.#counter.value) {
+      this.#counter.decrement();
+      cell.toggleFlag();
+    }
   };
 
   #setStyles() {
